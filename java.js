2194421952ac40729e47cdf022601ca8ec345771
ShubhamJoshii <shubhamjoshii676@gmail.com>
2022-06-28 23:30:45 +0530

    Portfolio

---
+++ java.js
@@ -0,0 +1,12 @@
+// burger= document.querySelector('.burger');
+// navbar= document.querySelector('.navbar');
+// nav2= document.querySelector('.nav2');
+// navli=document.querySelector('.navbar ul');
+
+// burger.addEventListner('click',()=>{
+//     navli.classList.toggle('.ul-resp');
+//     nav2.classList.toggle('.ul-resp');
+
+// })
+
+
